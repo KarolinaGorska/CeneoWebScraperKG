0a4555ff2b7768771aeff5b817262534effecd12
KarolinaGorska <231712@student.uek.krakow.pl>
2024-05-27 14:46:04 +0200

    2024-05-27 KG

--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,12 @@
 from app import app
-from flask import render_template, request, redirect, url_for
-import requests
+from flask import render_template, request, redirect, url_for, send_file
 from bs4 import BeautifulSoup
+import pandas as pd
+import numpy as np
+import requests
 import json
 import os
+import io
 from . import utils
 
 
@@ -22,6 +25,7 @@ def extract():
             page_dom = BeautifulSoup(response.text, "html.parser")
             opinions_count = utils.extract(page_dom, "a.product-review__link > span")
             if opinions_count:
+                product_name = utils.extract(page_dom, "h1.product-top__product-info__name > span")
                 all_opinions = []
                 while(url):
                     response = requests.get(url)
@@ -41,6 +45,23 @@ def extract():
                     os.makedirs("opinions")
                 with open(f"opinions/{product_id}.json", "w", encoding="UTF-8") as jf:
                     json.dump(all_opinions, jf,indent=4, ensure_ascii=False)
+                opinions = pd.DataFrame.from_dict(all_opinions)
+                opinions.stars = opinions.stars.apply(lambda s: s.split("/")[0].replace(",",".")).astype(float)
+                opinions.recommendation = opinions.recommendation.apply(lambda r: "Brak rekomendacji" if r is None else r)
+                stats = {
+                    'product_id': product_id,
+                    'product_name': product_name,
+                    'opinions_count':  opinions.shape[0],  #len(opinions) opinions.index.size
+                    'pros_count': int(opinions.pros.apply(lambda p: None if not p else p).count()),
+                    'cons_count': int(opinions.cons.apply(lambda c: None if not c else c).count()),
+                    'average_stars': opinions.stars.mean(),
+                    'stars_distribution': opinions.stars.value_counts().reindex(list(np.arange(0,5.5,0.5)), fill_value=0).to_dict,
+                    'recommendations_distribution': opinions.recommendation.value_counts(dropna=False).reindex(["Polecam", "Brak rekomendacji", "Nie polecam"], fill_value = 0).to_dict,
+                }
+                if not os.path.exists("app/products"):
+                    os.makedirs("app/products")
+                with open(f"app/products/{product_id}.json", "w", encoding="UTF-8") as jf:
+                    json.dump(all_opinions, jf,indent=4, ensure_ascii=False)
                 return redirect(url_for('product', product_id=product_id))
             return render_template("extract.html", error="Podany produkt nie ma żadnych opinii")
         return render_template("extract.html", error="Produkt o podanym kodzie nie istnieje")
@@ -50,7 +71,12 @@ def extract():
 
 @app.route('/products')
 def products():
-    return render_template("products.html")
+    products_list = [filename.split(".")[0] for filename in os.listdir("app/opinions")]
+    products = []
+    for product_id in products_list:
+        with open(f"app/products/{product_id}.json", "r", encoding="UTF-8") as jf:
+            products.append(json.load(jf))
+    return render_template("products.html", products=products)
 
 @app.route('/about')
 def about():
@@ -60,3 +86,17 @@ def about():
 def product(product_id):
     return render_template("product.html", product_id=product_id)
 
+@app.route('/product/download_json/<product_id>')
+def download_json(product_id):
+    return send_file(f"opinions/{product_id}.json", "text/json", as_attachment="True")
+
+@app.route('/product/download_csv/<product_id>')
+def download_csv(product_id):
+    opinions = pd.read_json(f"opinions/{product_id}.json")
+    opinions.stars = opinions.stars.apply(lambda s: "'"+s )
+    buffer = io.BytesIO(opinions.to_csv(sep=';', decimal=',', index= False).encode())
+    return send_file(buffer, "text/csv", as_attachment="True", download_name=f"{product_id}.csv")
+
+@app.route('/product/download_xlsx/<product_id>')
+def download_xlsx(product_id):
+    pass
